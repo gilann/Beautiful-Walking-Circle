142361f530712a6e01b8c3c7b78f558c86040ee1
Shivam Gupta <37578595+gilann@users.noreply.github.com>
2018-09-08 18:11:41 +0530

    Circle is ready to walk.

---
+++ sketch.js
@@ -0,0 +1,34 @@
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+}
+var x = 0;
+var y = 50;
+var r=255;
+var g= 200;
+var b= 200;
+var speed = 5;
+var col = 1;
+function draw() {
+	stroke(255,g,b,);
+	strokeWeight(5);
+	ellipse(x, y, 100, 100);
+	if(x>windowWidth/2){
+		col = -1;
+	}
+	if(x>=width){
+		speed= -5;
+		col=-1;
+		y = y+100;
+	}
+	x = x+speed;
+	if(x<1){
+		speed=5;
+		col=1;
+		y=y+100;
+	}
+	
+	//r = random(0,255);
+	g = random(0,255);
+	b = random(0,255);
+	
+}
